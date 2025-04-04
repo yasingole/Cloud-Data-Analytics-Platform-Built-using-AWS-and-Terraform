ead2068d6d9234e66075e4e6199886fc8889d34a
Yasin Gole <yasingole98@gmail.com>
2025-04-04 14:09:01 +0100

    removed reference to bastion

--- infrastructure/modules/ec2/variables.tf
+++ infrastructure/modules/ec2/variables.tf
@@ -20,12 +20,6 @@ variable "subnet_ids" {
   type        = list(string)
 }
 
-variable "bastion_sg_id" {
-  description = "Security group ID of the bastion host (if any)"
-  type        = string
-  default     = ""
-}
-
 variable "raw_data_bucket_arn" {
   description = "ARN of the raw data S3 bucket"
   type        = string
